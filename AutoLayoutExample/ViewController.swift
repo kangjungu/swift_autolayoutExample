50cfb0a7eedeb68ef9559d2891735af555134c83
KangJungu <KangJungu@JHJG.com>
2016-07-04 12:16:35 +0900

    Initial Commit

---
+++ AutoLayoutExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample
+//
+//  Created by JHJG on 2016. 7. 4..
+//  Copyright © 2016년 KangJungu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
